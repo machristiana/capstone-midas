aae84c5727b37fe1ecd57b375130a17218592492
Melissa Christiana <machristiana@ufl.edu>
2022-04-17 11:55:31 -0400

    Attempt modal pop up styling

---
+++ modal-popup.js
@@ -0,0 +1,11 @@
+// var popup = document.getElementById("mcjs")
+
+
+// dijit_TemplatedMixin_0
+
+
+window.onload = function() {
+    let myiFrame = document.getElementById("dijit_TemplatedMixin_0");
+    let doc = myiFrame.contentDocument;
+    doc.body.innerHTML = doc.body.innerHTML + '<style>.bannerContent_closeButton {left: 5px}</style>';
+}
