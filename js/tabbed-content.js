39c907058dc5e8b322e8e1f504addb9de18f35cc
Melissa Christiana <machristiana@ufl.edu>
2022-04-17 19:44:37 -0400

    Create individual eyeglasses page

---
+++ js/tabbed-content.js
@@ -0,0 +1,9 @@
+//https://www.cssscript.com/responsive-tabbed-content-tabs/
+
+var myTabs = tabs({
+    el: '#tabs',
+    tabNavigationLinks: '.c-tabs-nav__link',
+    tabContentContainers: '.c-tab'
+});
+
+myTabs.init();
